536b6416d9078b2e445fb264f629ebe92da8f76b
Gene Boggs <gene.boggs@gmail.com>
2016-09-25 15:00:55 -0700

    Modularize with iterate_plot()

--- bifurcation.R
+++ bifurcation.R
@@ -20,12 +20,16 @@ iterate <- function( equation, range, x_max ) {
     return(v)
 }
 
-# Set the range of the r values
-r_min <- 1
-r_max <- 4
+iterate_plot <- function( equation, r_min, r_max, x_max ) {
+    v <- iterate( equation = equation, range = seq( r_min, r_max, 0.01 ), x_max = x_max )
+
+    r <- seq( r_min, r_max, length.out = length(v) )
+
+    plot( r, v, type = 'p', cex  = 0.1, xlab = 'r', ylab = 'x' )
+}
 
 # The logistic equation
-# r * x * ( 1 - x )
+#iterate_plot( equation = 'r * x * ( 1 - x )', r_min = 1, r_max = 4, x_max = 30 )
 # Alternatives:
 # r - x ** 2
 # r ** ( 1 - x )
@@ -36,12 +40,4 @@ r_max <- 4
 # r ** sin(x)
 # r ** cos( 1 - x )
 # r * cos(x) * ( 1 - sin(x) )
-# r * exp(x) * ( 1 - exp(x) )
-v <- iterate( equation = 'r * x * ( 1 - x )', range = seq( r_min, r_max, 0.01 ), x_max = 30 )
-
-# Recompute our range in terms of the equation iteration.
-r <- seq( r_min, r_max, length.out = length(v) )
-
-#png(file = 'bifurcation.png')
-plot( r, v, type = 'p', cex  = 0.1, xlab = 'r', ylab = 'x' )
-#dev.off()
+iterate_plot( equation = 'r * exp(x) * ( 1 - exp(x) )', r_min = 0.5, r_max = 3, x_max = 30 )
