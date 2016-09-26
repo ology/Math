096d316aa92a17e5806f44950a6a588a0dfe1c24
Gene Boggs <gene.boggs@gmail.com>
2016-09-25 18:03:39 -0700

    Add another alternate equation

--- bifurcation.R
+++ bifurcation.R
@@ -40,4 +40,5 @@ iterate_plot <- function( r_min, r_max, equation ) {
 #iterate_plot( r_min = 2, r_max = 8, equation = 'r ** cos( 1 - x )' )
 #iterate_plot( r_min = 0, r_max = 5, equation = 'r * cos(x) * ( 1 - sin(x) )' )
 #iterate_plot( r_min = 0.5, r_max = 3, equation = 'r * exp(x) * ( 1 - exp(x) )' )
-iterate_plot( r_min = 0, r_max = 2, equation = 'r - x ** 2' )
+#iterate_plot( r_min = 0, r_max = 2, equation = 'r - x ** 2' )
+iterate_plot( r_min = 2.4, r_max = 4, equation = 'r * x * ( 1 - x * r )' )
