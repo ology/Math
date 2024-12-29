91e2c9a65c0c16d695fd0d52a22d9fd52620c265
Gene Boggs <gene@ology.net>
2024-12-29 12:34:24 -0800

    Fresh

---
+++ primes/date-primes.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/env perl
+
+# Find dates that are prime
+
+use strict;
+use warnings;
+
+use Data::Dumper::Compact qw(ddc);
+use Math::Prime::XS qw(is_prime);
+use Time::Moment ();
+
+my $start_year = shift || 2025;
+my $end_year   = shift || 2030;
+
+my $year  = $start_year;
+my $month = 1;
+my $day   = 1;
+
+my $tm = Time::Moment->new(
+    year  => $year,
+    month => $month,
+    day   => $day,
+);
+
+my $prime_stamps = {};
+my $counts       = {};
+
+while ($year <= $end_year) {
+    $month = sprintf '%02d', $tm->month;
+    $day   = sprintf '%02d', $tm->day_of_month;
+    my $stamp = $year . $month . $day;
+    if (is_prime($stamp)) {
+        push $prime_stamps->{$year}{$month}->@*, $stamp;
+        $counts->{$year}++;
+    }
+    $tm = $tm->plus_days(1);
+    $year = $tm->year;
+}
+print ddc $prime_stamps;
+print ddc $counts;
