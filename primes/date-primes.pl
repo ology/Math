77ca6ef5471733e5c8689064dfee8df4332347aa
Gene Boggs <gene@ology.net>
2024-12-30 11:49:06 -0800

    Days

--- primes/date-primes.pl
+++ primes/date-primes.pl
@@ -27,6 +27,7 @@ my $tm = Time::Moment->new(
 my $prime_stamps = {};
 my $by_year      = {};
 my $by_month     = {};
+my $days         = {};
 
 while ($year <= $end_year) {
     $month = sprintf '%02d', $tm->month;
@@ -36,6 +37,7 @@ while ($year <= $end_year) {
         push $prime_stamps->{$year}{$month}->@*, $stamp;
         $by_year->{$year}++;
         $by_month->{$year}{$month}++;
+        $days->{$day}++;
     }
     $tm = $tm->plus_days(1);
     $year = $tm->year;
@@ -50,9 +52,14 @@ for my $y (sort keys %$by_month) {
 
 print "\nPrime dates: ", ddc $prime_stamps;
 # print "\nMonth by year count: ", ddc $by_month;
-print "\nMonth counts: ", ddc $months;
+# print "\nMonth counts: ", ddc $months;
+print "\nBy month:\n";
 for my $m (sort keys $months->%*) {
     printf "$m: Mean: %.3f, Stddev: %.3f, Median: %.3f, Total: %d\n",
         mean($months->{$m}), stddev($months->{$m}), median($months->{$m}), sum0($months->{$m}->@*);
+}
+print "\nBy day:\n";
+for my $d (sort keys $days->%*) {
+    printf "$d: %d\n", $days->{$d};
 }
 print "\nBy year count: ", ddc $by_year;
